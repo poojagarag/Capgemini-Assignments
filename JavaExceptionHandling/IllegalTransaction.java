3fc935fcc34a8bac3345c7a3849a16ac9459ac9e
pgarag <p.r.garag@gmail.com>
2021-11-02 00:19:01 +0530

    Initial commit

---
+++ JavaExceptionHandling/IllegalTransaction.java
@@ -0,0 +1,34 @@
+import java.util.*;
+public class IllegalTransaction {
+        static void fun() throws IllegalAccessException {
+            double balance = 0, depositAmount, withdrawAmount;
+
+            Scanner dep = new Scanner(System.in);
+            System.out.println("Enter Deposit an amount");
+            depositAmount = dep.nextInt();
+            balance += depositAmount;
+
+            Scanner wd = new Scanner(System.in);
+            System.out.println("Enter Withdraw an amount ");
+            withdrawAmount = wd.nextInt();
+            balance -= withdrawAmount;
+
+
+            if ( withdrawAmount < -1) {
+                throw new IllegalAccessException();
+            } else
+                System.out.println("SUCCESSFULLY WITHDRAWN : " + withdrawAmount);
+            System.out.println("ACCOUNT BALANCE AFTER  THE TRANSACTION : " + balance);
+        }
+
+        public static void main(String args[]) {
+
+            try {
+                fun();
+            } catch (IllegalAccessException e) {
+                System.out.println(" WARNING :ILLEGAL BANK TRANSACTION EXCEPTION ");
+            }
+        }
+
+    }
+
