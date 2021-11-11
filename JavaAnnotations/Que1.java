d923e78a74ac1171778799c7cacf38ad28b005ff
pgarag <p.r.garag@gmail.com>
2021-11-12 01:04:44 +0530

    Initial commit

---
+++ JavaAnnotations/Que1.java
@@ -0,0 +1,16 @@
+
+public class Que1 {
+
+        public static void main(String[] args) {
+
+        }
+    }
+    @interface Test{
+        String test_case();
+    }
+
+    @Test(test_case = "1")
+    class A{
+
+    }
+
